68009a64c0763ae22caff902f831dd1692ccbead
Juan J. Martinez <jjm@usebox.net>
2020-05-17 07:37:10 +0100

    Sprite class; also use Element to allow a Canvas

--- game/src/net/usebox/game/Drawable.scala
+++ game/src/net/usebox/game/Drawable.scala
@@ -3,7 +3,10 @@ package net.usebox.game
 import org.scalajs.dom
 
 trait Drawable {
-  def image: dom.html.Image
+  def image: dom.html.Element
   def x: Double
   def y: Double
 }
+
+case class Sprite(var image: dom.html.Element, x: Double, y: Double)
+    extends Drawable
