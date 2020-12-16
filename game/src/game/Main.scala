32992c942dc9f9acae2f8c73f50dfa38b4180461
Juan J. Martinez <jjm@usebox.net>
2020-12-16 21:39:21 +0000

    Formatting

--- game/src/game/Main.scala
+++ game/src/game/Main.scala
@@ -8,10 +8,9 @@ object Main {
   implicit def executionContext =
     scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
 
-  def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit =
     dom.document.addEventListener(
       "DOMContentLoaded",
       { (e: dom.Event) => new MyLoader("game").run }
     )
-  }
 }
